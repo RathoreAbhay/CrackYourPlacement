d7b91d61cfb43cb45ff31b1bf24f4dcd7f4b5486
Abhay Rathore <114696547+RathoreAbhay@users.noreply.github.com>
2024-07-21 00:04:49 +0530

    Initial commit

---
+++ RemoveDuplicatesfromSortedArray.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int prev =0;
+        if(nums.size()==0){
+            return 0;
+        }
+        for(int curr=1;curr<nums.size();curr++){
+            if(nums[curr] != nums[prev]){
+                prev++;
+                nums[prev]=nums[curr];
+            }
+        }
+        return prev+1;
+    }
+};
