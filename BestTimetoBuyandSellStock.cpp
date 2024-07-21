7e9cb3fb0892b87da92ec3a8c8936021fe86ea87
Abhay Rathore <114696547+RathoreAbhay@users.noreply.github.com>
2024-07-22 00:36:31 +0530

    Initial commit

---
+++ BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int minSoFar = prices[0];
+        if (prices.empty()) {
+            return 0;
+        }
+
+        for (int i = 1; i < prices.size(); i++) {
+            minSoFar = min(minSoFar, prices[i]);
+            int profit = prices[i] - minSoFar;
+            maxProfit = max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+};
