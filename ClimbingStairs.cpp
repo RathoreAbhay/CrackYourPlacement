9522c19554e5c96afc1f160778691566fb936feb
Abhay Rathore <114696547+RathoreAbhay@users.noreply.github.com>
2024-07-31 23:41:19 +0530

    Initial commit

---
+++ ClimbingStairs.cpp
@@ -0,0 +1,11 @@
+int climbStairs(int n) {
+    int prev = 1;
+    int prev2 = 1;
+    
+    for (int i = 2; i <= n; i++) {
+        int curr = prev + prev2;
+        prev2 = prev;
+        prev = curr;
+    }
+  return prev;
+}
